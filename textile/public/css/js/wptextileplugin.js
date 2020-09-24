16b77c5327e9cc8fff3e5cc8d0b8df3efdcd9ab0
luckyr13 <richie.isc@gmail.com>
2020-09-23 19:32:24 -0500

    Add files via upload

---
+++ textile/public/css/js/wptextileplugin.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($) {
+    var data = TEXTILE_AJAX_OBJ ? TEXTILE_AJAX_OBJ : {};
+    var textile = new WPTextilePlugin(data);
+
+});
+
+var WPTextilePlugin = (
+    function($) {
+        var constructor = function(data) {
+           
+        }
+
+
+        return constructor;
+    }
+)(jQuery);
